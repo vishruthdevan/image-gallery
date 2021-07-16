6165f3438ba03055769b28280d0aa5e3f789848e
Vishruth Devan <vishruthdevan234@gmail.com>
2021-07-16 22:11:17 +0530

    Correct errors in urls.py

--- mysite/photos/views.py
+++ mysite/photos/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from django.urls import reverse
 # Create your views here.
 
 def gallery(request):
+    print(reverse('photo', args=['69']))
     return render(request, 'photos/gallery.html')
 
 def addPhoto(request):
