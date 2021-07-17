84e7d49c888de8f1477adc4866e0b2d9800b7bb3
Vishruth Devan <vishruthdevan234@gmail.com>
2021-07-17 23:47:27 +0530

    Configure static files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,8 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
