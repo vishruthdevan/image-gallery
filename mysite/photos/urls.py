0d434cee0b179b0e881510dc91376eb05cf16f9e
Vishruth Devan <vishruthdevan234@gmail.com>
2021-07-19 20:58:12 +0530

    Enable delete category

--- mysite/photos/urls.py
+++ mysite/photos/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('photo/<str:pk>/', views.viewPhoto, name='photo'),
     path('photo/delete/<str:pk>/', views.deletePhoto, name='delete'),
+    path('cat/delete/', views.deleteCat, name='delete_cat'),
     path('add/', views.addPhoto, name='add'),
+
 ]
