d8466e5d56ed49a4b16e7337d358b44cb05f084c
Vishruth Devan <vishruthdevan@gmail.com>
2025-06-28 11:56:39 -0400

    Add delete image functionality

--- mysite/photos/urls.py
+++ mysite/photos/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('photo/<str:pk>/', views.viewPhoto, name='photo'),
+    path('photo/delete/<str:pk>/', views.deletePhoto, name='delete'),
     path('add/', views.addPhoto, name='add'),
 ]
