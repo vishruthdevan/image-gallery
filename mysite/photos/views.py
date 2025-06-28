9bb19ef0b554eaae6034802ed6bec39de9c964e3
Vishruth Devan <vishruthdevan@gmail.com>
2025-06-28 11:56:38 -0400

    Render data in templates

--- mysite/photos/views.py
+++ mysite/photos/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.urls import reverse
+from .models import Category, Photo
 # Create your views here.
 
 def gallery(request):
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {'categories' : categories, 'photos' : photos}
     print(reverse('photo', args=['69']))
-    return render(request, 'photos/gallery.html')
+    return render(request, 'photos/gallery.html', context)
 
 def addPhoto(request):
     return render(request, 'photos/add.html')
 
 def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo':photo})
